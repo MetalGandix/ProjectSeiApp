adc4196f97b5ac38094c7ac2e7a20ee84ee8059c
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-04-25 21:49:04 +0200

    Aggiunte le prime pagine Angular

--- front-end-seiapp/src/app/app-routing.module.ts
+++ front-end-seiapp/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OptionComponent } from './option/option.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'option', component: OptionComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
