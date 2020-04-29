7a66542ed63265fd15419cf34cb34f94f185d816
brianbernardini <44262397+brianbernardini@users.noreply.github.com>
2020-04-29 17:10:46 +0200

    Creata Classe Caratteristiche Qualitative

---
+++ seiapp/src/main/java/com/project/seiapp/entità/CaratterisitcheQualitative.java
@@ -0,0 +1,33 @@
+package com.project.seiapp.entità;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CaratterisitcheQualitative")
+public class CaratterisitcheQualitative {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String CaratteristicheQualitative;
+
+    public String getCaratteristicheQualitative() {
+        return CaratteristicheQualitative;
+    }
+
+    public void setCaratteristicheQualitative(String caratteristicheQualitative) {
+        CaratteristicheQualitative = caratteristicheQualitative;
+    }
+
+
+    }
+
+
+
