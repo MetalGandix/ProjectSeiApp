032b3d44653c8d1a1dedd068deeb33ab61d4f2c8
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-02 15:53:40 +0200

    Prova per visualizzare array

--- front-end-seiapp/src/app/individuazione-interventi/individuazione-interventi.component.ts
+++ front-end-seiapp/src/app/individuazione-interventi/individuazione-interventi.component.ts
@@ -16,21 +16,53 @@ export class IndividuazioneInterventiComponent implements OnInit {
   verticale: StrutturaVerticale[];
   caratteristica: CaratteristicheQualitative[];
 
-  constructor(private strutturaVerticaleService: StrutturaVerticaleService,
-              private route: ActivatedRoute,
-              private router: Router) {
-              this.struttura = new StrutturaVerticale();
 
-               }
+
+  constructor(private strutturaVerticaleService: StrutturaVerticaleService,
+    private route: ActivatedRoute,
+    private router: Router) {
+    this.struttura = new StrutturaVerticale();
+  }
+
+
+  visualizzaArrayVerticale() {
+   this.verticale[0]
+  }
 
   ngOnInit() {
-    this.strutturaVerticaleService.getStrutturaVerticale().subscribe(data=>{
+    this.strutturaVerticaleService.getStrutturaVerticale().subscribe(data => {
       this.verticale = data;
     })
-      
-      this.strutturaVerticaleService.getCaratteristicheQualitative().subscribe(data=>{
-        this.caratteristica = data;
+
+    this.strutturaVerticaleService.getCaratteristicheQualitative().subscribe(data => {
+      this.caratteristica = data;
     })
   }
+
+
+
+  onSubmit() {
+    var oggettoStrutturaVerticale: StrutturaVerticale = new StrutturaVerticale();
+    if (oggettoStrutturaVerticale.strutturaVerticale == "Struttura ad arco in muratura") {
+      console.log("Hai selezionato struttura ad arco in muratura");
+    }
+    if (oggettoStrutturaVerticale.strutturaVerticale == "Struttura a pareti portanti in muratura piena") {
+      console.log("Hai selezionato struttura a pareti portanti in muratura piena");
+    }
+    if (oggettoStrutturaVerticale.strutturaVerticale == "Struttura a pareti portanti in muratura a sacco") {
+      console.log("Hai selezionato struttura a pareti portanti in muratura a sacco");
+    }
+
+  }
+
+  /* 
+  PROVE JAVASCRIPT
+
+  prova(){
+    this.verticale.forEach(this.funzioneProva);
+  }
+  funzioneProva(item, index){
+    console.log(document.getElementById("demo").innerHTML += index + ":" + item + "");
+  }*/
 
 }
