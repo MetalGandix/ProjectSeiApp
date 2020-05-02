12ac520d70345369bae6cffe4838ad401655e645
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-02 19:20:10 +0200

    Aggiunti if

--- front-end-seiapp/src/app/individuazione-interventi/individuazione-interventi.component.ts
+++ front-end-seiapp/src/app/individuazione-interventi/individuazione-interventi.component.ts
@@ -12,18 +12,14 @@ import { CaratteristicheQualitative } from '../classi-servizi/classes/caratteris
 })
 export class IndividuazioneInterventiComponent implements OnInit {
 
-  @Input('verticale') verticale: StrutturaVerticale[]; 
-  @Input('caratteristica') caratteristica: CaratteristicheQualitative[];
+  verticale: StrutturaVerticale[]; 
+  caratteristica: CaratteristicheQualitative[];
 
-  @Output() valori: EventEmitter<string> = new EventEmitter();
+  valori: EventEmitter<string> = new EventEmitter();
   
   constructor(private strutturaVerticaleService: StrutturaVerticaleService,
     private route: ActivatedRoute,
     private router: Router) {
-  }
-
-  selectItem(value) {
-    this.valori.emit(value);
   }
 
  prova(){
@@ -53,10 +49,52 @@ export class IndividuazioneInterventiComponent implements OnInit {
   if(valore1.valueOf() == 1 && valore2.valueOf() == 0){
     alert("Il metodo selezionato è questo: 1-0 ")
   }
+  if(valore1.valueOf() == 1 && valore2.valueOf() == 1){
+    alert("Il metodo selezionato è questo: 1-1 ")
+  }
+  if(valore1.valueOf() == 1 && valore2.valueOf() == 2){
+    alert("Il metodo selezionato è questo: 1-2 ")
+  }
+  if(valore1.valueOf() == 1 && valore2.valueOf() == 3){
+    alert("Il metodo selezionato è questo: 1-3 ")
+  }
+  if(valore1.valueOf() == 1 && valore2.valueOf() == 4){
+    alert("Il metodo selezionato è questo: 1-4 ")
+  }
+  if(valore1.valueOf() == 1 && valore2.valueOf() == 5){
+    alert("Il metodo selezionato è questo: 1-5 ")
+  }
   if(valore1.valueOf() == 2 && valore2.valueOf() == 0){
     alert("Il metodo selezionato è questo: 2-0 ")
   }
-}
+  if(valore1.valueOf() == 2 && valore2.valueOf() == 1){
+    alert("Il metodo selezionato è questo: 2-1 ")
+  }
+}  
+
+  ngOnInit() {
+    this.strutturaVerticaleService.getStrutturaVerticale().subscribe(data => {
+      this.verticale = data;
+    })
+
+    this.strutturaVerticaleService.getCaratteristicheQualitative().subscribe(data => {
+      this.caratteristica = data;
+    })
+  }
+
+
+
+ 
+
+  /* 
+  PROVE JAVASCRIPT
+
+  prova(){
+    this.verticale.forEach(this.funzioneProva);
+  }
+  funzioneProva(item, index){
+    console.log(document.getElementById("demo").innerHTML += index + ":" + item + "");
+  }*/
 
   //Trasformo l'array di oggetti in un array di stringhe
   visualizzaArrayVerticale() {
@@ -75,18 +113,6 @@ export class IndividuazioneInterventiComponent implements OnInit {
 
   }
 
-  ngOnInit() {
-    this.strutturaVerticaleService.getStrutturaVerticale().subscribe(data => {
-      this.verticale = data;
-    })
-
-    this.strutturaVerticaleService.getCaratteristicheQualitative().subscribe(data => {
-      this.caratteristica = data;
-    })
-  }
-
-
-
   onSubmit() {
     var oggettoStrutturaVerticale: StrutturaVerticale = new StrutturaVerticale();
     if (oggettoStrutturaVerticale.strutturaVerticale == "Struttura ad arco in muratura") {
@@ -100,15 +126,5 @@ export class IndividuazioneInterventiComponent implements OnInit {
     }
 
   }
-
-  /* 
-  PROVE JAVASCRIPT
-
-  prova(){
-    this.verticale.forEach(this.funzioneProva);
-  }
-  funzioneProva(item, index){
-    console.log(document.getElementById("demo").innerHTML += index + ":" + item + "");
-  }*/
 
 }
