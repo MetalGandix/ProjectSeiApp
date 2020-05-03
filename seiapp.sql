70eac3b92745c586a84d56171da1c0201c28ed81
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-03 18:57:15 +0200

    Aggiornato il db
    Aggiornato il db con l'username: leonardo e la password: password.
    Provato ora con PostMan e funziona bene.

--- seiapp.sql
+++ seiapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 01, 2020 alle 18:10
+-- Creato il: Mag 03, 2020 alle 18:56
 -- Versione del server: 10.4.11-MariaDB
 -- Versione PHP: 7.4.5
 
@@ -20,18 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `seiapp`
 --
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `admin`
---
-
-CREATE TABLE `admin` (
-  `id` bigint(20) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -54,6 +42,43 @@ INSERT INTO `caratterisitche_qualitative` (`id`, `caratteristiche_qualitative`) 
 (16, 'Presenza di numerose nicchie'),
 (17, 'Pareti di elevate dimensioni non controventate'),
 (18, 'Pannelli murari male ammorsati tra loro');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `hibernate_sequence`
+--
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `role`
+--
+
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `role`
+--
+
+INSERT INTO `role` (`id`, `description`, `name`) VALUES
+(1, 'Amministratore del sito', 'Admin'),
+(2, 'Utente del sito', 'User');
 
 -- --------------------------------------------------------
 
@@ -84,7 +109,26 @@ INSERT INTO `struttura_verticale` (`id`, `struttura_verticale`) VALUES
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `username`) VALUES
+(1, 'leonardo.mogianesi@studenti.unicam.it', '$2a$04$VLfuPg3CHUwNXZm.AsVdOu32OV680pSmfo.kvcp/59/AhMt3nZr0m', 'leonardo');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `role_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -92,15 +136,15 @@ CREATE TABLE `user` (
 --
 
 --
--- Indici per le tabelle `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indici per le tabelle `caratterisitche_qualitative`
 --
 ALTER TABLE `caratterisitche_qualitative`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `role`
+--
+ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -116,14 +160,15 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- Indici per le tabelle `user_roles`
 --
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_id`,`role_id`),
+  ADD KEY `FKrhfovtciq1l558cw6udg0h0d3` (`role_id`);
 
 --
--- AUTO_INCREMENT per la tabella `admin`
+-- AUTO_INCREMENT per le tabelle scaricate
 --
-ALTER TABLE `admin`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `caratterisitche_qualitative`
@@ -141,7 +186,18 @@ ALTER TABLE `struttura_verticale`
 -- AUTO_INCREMENT per la tabella `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `FK55itppkw3i07do3h7qoclqd4k` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FKrhfovtciq1l558cw6udg0h0d3` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
