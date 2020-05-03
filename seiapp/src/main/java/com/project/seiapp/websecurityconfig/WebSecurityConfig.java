4194fdc21c4f6e654a2fb8d9046d5e9bc53ce6ba
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-03 18:52:19 +0200

    Login Role Based finito, va riguardato in alcune parti.
    Login role base con il token JWT, sembra funzionare

---
+++ seiapp/src/main/java/com/project/seiapp/websecurityconfig/WebSecurityConfig.java
@@ -0,0 +1,79 @@
+package com.project.seiapp.websecurityconfig;
+
+import javax.annotation.Resource;
+
+import com.project.seiapp.jwt.JwtAuthenticationEntryPoint;
+import com.project.seiapp.jwt.JwtRequestFilter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Resource(name = "userService")
+	@Autowired
+	private UserDetailsService jwtUserDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jwtUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// AuthManager per le credenziali
+		// BCryptPasswordEncoder per decifrare la password SHA-512
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		// We don't need CSRF for this example
+		httpSecurity.csrf().disable()
+				//Serve per autenticare queste pagine senza uso di alcun token (più avanti le toglieremo)
+				.authorizeRequests().antMatchers("/authenticate", "/register", "user-form", "/existUser/{username}","/strutturaVerticale", "/caratteristicheQualitative").permitAll().
+				
+				anyRequest().authenticated().and().
+				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	httpSecurity
+				.logout().logoutSuccessUrl("/login");
+
+		//Aggiunge un filtro per validare il token ad ogni richiesta, dato che il token sarà sempre diverso.
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
