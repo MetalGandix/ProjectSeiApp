bc3688c38ba614c7cdb65e24b7330a2211cd8af9
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-10 19:46:17 +0200

    FUNZIONA LA SBARRA

--- front-end-seiapp/src/app/edificio/edificio-singolo/edificio-singolo.component.ts
+++ front-end-seiapp/src/app/edificio/edificio-singolo/edificio-singolo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EdificioService } from 'src/app/classi-servizi/service/edificio.service';
 import { TipoEdificio } from 'src/app/classi-servizi/classes/tipo-edificio';
@@ -10,8 +10,10 @@ import { TipoEdificio } from 'src/app/classi-servizi/classes/tipo-edificio';
 })
 export class EdificioSingoloComponent implements OnInit {
 
-  bomb: number
+
   edificioSingolo: TipoEdificio[];
+
+  valori: EventEmitter<string> = new EventEmitter();
 
   constructor(
     private route: ActivatedRoute,
@@ -26,22 +28,24 @@ export class EdificioSingoloComponent implements OnInit {
     })
   }
 
-  
-
   outputSelezione(){
     var select1 = (<HTMLInputElement>document.getElementById("edificioSingolo")).value;
     var valore1 = parseInt(select1)
     //Scrive i valori presi in input document.write(select1,select2);
-    if(valore1.valueOf() == 1){
-      alert("Il tipo di edificio non può essere selezionato")
-    }
-    if(valore1.valueOf() == 3){
-      alert("Il tipo di edificio non può essere selezionato")
+    if(valore1.valueOf() == 0){
+      this.router.navigate["/edificio-singolo-q"]
     }
     if(valore1.valueOf() == 2){
       this.router.navigate["/edificio-singolo-q"]
     }
+    if(valore1.valueOf() == 4){
+      this.router.navigate["/edificio-singolo-q"]
+    }
+    if(valore1.valueOf() == 5){
+      this.router.navigate["/edificio-singolo-q"]
+    }
+    if(valore1.valueOf() == 6){
+      this.router.navigate["/edificio-singolo-q"]
+    }
   }
-
-
 }
