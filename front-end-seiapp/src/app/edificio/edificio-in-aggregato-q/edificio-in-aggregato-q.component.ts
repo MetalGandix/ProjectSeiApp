1e1c6cdeba9676ae45b381ec9e2052e439326ff1
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-13 23:41:53 +0200

    Gli if funzionano con il punteggio
    L'unico difetto è che non il contatore si aggiorna solo dopo aver cliccato, quindi bisogna cliccare 2 volte

--- front-end-seiapp/src/app/edificio/edificio-in-aggregato-q/edificio-in-aggregato-q.component.ts
+++ front-end-seiapp/src/app/edificio/edificio-in-aggregato-q/edificio-in-aggregato-q.component.ts
@@ -22,7 +22,7 @@ export class EdificioInAggregatoQComponent implements OnInit {
   muratura: EdificioInaggregatoComponent;
   edificioFiltro: EdificioInAggregato[];
   quality: Quality[] = [];
-  totalePunteggio: number
+  totalePunteggio: Number
   varEmp: Number
 
 
@@ -373,9 +373,20 @@ export class EdificioInAggregatoQComponent implements OnInit {
       alert("La classe di vulnerabilità è V6")
     }
     if (this.varEmp == 3) {
-      if (this.totalePunteggio <= 50) {
-        alert("La classe di vulnerabilità è V5")
+      if (this.totalePunteggio < 50) {
+      alert("La classe di vulnerabilità è V5")
+        }else{
+      alert("La classe di vulnerabilità è V6")
+        }
+      }
+      if(this.varEmp == 5){
+        if(this.totalePunteggio < 30){
+          alert("La classe di vulnerabilità è V4")
+        }else if(this.totalePunteggio >= 60){
+          alert("La classe di vulnerabilità è V6")
+        }else if(this.totalePunteggio >= 30 && this.totalePunteggio <= 60){
+          alert("La classe di vulnerabilità è V5")
+        }
       }
     }
-  }
 }
