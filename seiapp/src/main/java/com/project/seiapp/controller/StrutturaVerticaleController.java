1dda899cdd37524746d83160ff6445f3a76aac76
brianbernardini <44262397+brianbernardini@users.noreply.github.com>
2020-05-22 18:03:20 +0200

    tolte le secondary table

--- seiapp/src/main/java/com/project/seiapp/controller/StrutturaVerticaleController.java
+++ seiapp/src/main/java/com/project/seiapp/controller/StrutturaVerticaleController.java
@@ -2,7 +2,7 @@ package com.project.seiapp.controller;
 
 import java.util.List;
 
-import com.project.seiapp.entità.StrutturaVerticale;
+import com.project.seiapp.entità.elementiStruttura.StrutturaVerticale;
 import com.project.seiapp.repository.StrutturaVerticaleRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,31 +22,6 @@ public class StrutturaVerticaleController {
 
     @GetMapping("/strutturaVerticale")
     public List<StrutturaVerticale> getStruttura(){
-    return (List<StrutturaVerticale>) verticaleRepository.findAll();
-    }
-
-    @GetMapping("/struttureDiElevazioniOrizzontali")
-    public List<StrutturaVerticale> getStrutturaDiElevazioniOrizzontali(){
-    return (List<StrutturaVerticale>) verticaleRepository.findAll();
-    }
-
-    @GetMapping("/struttureDiElevazioniInclinate")
-    public List<StrutturaVerticale> getstruttureDiElevazioniInclinate(){
-    return (List<StrutturaVerticale>) verticaleRepository.findAll();
-    }
-
-    @GetMapping("/struttureDiElevazioneSpaziali")
-    public List<StrutturaVerticale> getstruttureDiElevazioneSpaziali(){
-    return (List<StrutturaVerticale>) verticaleRepository.findAll();
-    }
-
-    @GetMapping("/infissiEsterniVerticali")
-    public List<StrutturaVerticale> getinfissiEsterniVerticali(){
-    return (List<StrutturaVerticale>) verticaleRepository.findAll();
-    }
-
-    @GetMapping("/elementiNonStrutturali")
-    public List<StrutturaVerticale> getelementiNonStrutturali(){
     return (List<StrutturaVerticale>) verticaleRepository.findAll();
     }
 
