d818ddc39dc51b0f3b7d6aa105b764c799cbc53c
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-05-26 16:04:21 +0200

    Join di StrutturaVerticale con CaratteristicheQualitative

--- seiapp/src/main/java/com/project/seiapp/entità/CaratteristicheQualitative.java
+++ seiapp/src/main/java/com/project/seiapp/entità/CaratteristicheQualitative.java
@@ -1,11 +1,15 @@
 package com.project.seiapp.entità;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.project.seiapp.entità.elementiStruttura.StrutturaVerticale;
 
 @Entity
 @Table(name = "CaratterisitcheQualitative")
@@ -14,6 +18,9 @@ public class CaratteristicheQualitative {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private StrutturaVerticale struttura;
 
     @Column
     private String CaratteristicheQualitative;
@@ -24,6 +31,22 @@ public class CaratteristicheQualitative {
 
     public void setCaratteristicheQualitative(String caratteristicheQualitative) {
         this.CaratteristicheQualitative = caratteristicheQualitative;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public StrutturaVerticale getStruttura() {
+        return struttura;
+    }
+
+    public void setStruttura(StrutturaVerticale struttura) {
+        this.struttura = struttura;
     }
 
 
--- seiapp/src/main/java/com/project/seiapp/entità/elementiStruttura/StrutturaVerticale.java
+++ seiapp/src/main/java/com/project/seiapp/entità/elementiStruttura/StrutturaVerticale.java
@@ -1,11 +1,18 @@
 package com.project.seiapp.entità.elementiStruttura;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import com.project.seiapp.entità.CaratteristicheQualitative;
 
 @Entity
 @Table(name = "strutturaVerticale")
@@ -16,6 +23,10 @@ public class StrutturaVerticale {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable 
+    private List<CaratteristicheQualitative> carQuality;
+
     @Column
     private String strutturaVerticale;
 
@@ -25,6 +36,22 @@ public class StrutturaVerticale {
 
     public void setStrutturaVerticale(String strutturaVerticale) {
         this.strutturaVerticale = strutturaVerticale;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<CaratteristicheQualitative> getCarQuality() {
+        return carQuality;
+    }
+
+    public void setCarQuality(List<CaratteristicheQualitative> carQuality) {
+        this.carQuality = carQuality;
     }
 
     
