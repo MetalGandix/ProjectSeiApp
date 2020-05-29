d5b7365566de643bd10262b851c39804d7ea9511
brianbernardini <44262397+brianbernardini@users.noreply.github.com>
2020-05-29 14:42:41 +0200

    Entity, reposotory e controller per i meccanismi

---
+++ seiapp/src/main/java/com/project/seiapp/controller/MeccanismiController.java
@@ -0,0 +1,26 @@
+package com.project.seiapp.controller;
+
+import java.util.List;
+
+import com.project.seiapp.entità.Meccanismi;
+import com.project.seiapp.repository.interventirepository.MeccanismiRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class MeccanismiController {
+    public MeccanismiController(MeccanismiRepository meccanismiRepository){
+        this.meccanismiRepository = meccanismiRepository;
+    }
+
+    private MeccanismiRepository meccanismiRepository;
+
+    @GetMapping("/meccanismi")
+    public List<Meccanismi> getMeccanismi(){
+        return (List<Meccanismi>) meccanismiRepository.findAll();
+    }
+    
+}
---
+++ seiapp/src/main/java/com/project/seiapp/repository/interventirepository/MeccanismiRepository.java
@@ -0,0 +1,9 @@
+package com.project.seiapp.repository.interventirepository;
+
+import com.project.seiapp.entità.Meccanismi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeccanismiRepository extends JpaRepository<Meccanismi, String> {
+    
+}
