cfd3b82951dc6d88f2ed1d8ddf0d29d898abb550
Leonardo Mogianesi <50453124+MetalGandix@users.noreply.github.com>
2020-08-11 20:26:17 +0200

    Rimosso completamente la sicurezza da spring

--- seiapp/src/main/java/com/project/seiapp/repository/interventirepository/InterventoA2Repository.java
+++
@@ -1,9 +0,0 @@
-package com.project.seiapp.repository.interventirepository;
-
-import com.project.seiapp.entità.interventi.CreazioneInterventoA2;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterventoA2Repository extends JpaRepository<CreazioneInterventoA2, String> {
-    
-}
